dbfd65d4aa23db7b8c21ecc3ff443079c3eddbd7
PrabinRai <chokhang777@gmail.com>
2015-02-26 15:28:02 +0545

    Assignment is Completed

--- q2.cpp
+++ q2.cpp
@@ -1,15 +1,22 @@
 /*
  Program: Question 2 from quiz 1.
- Name : 
- Roll: 
- Output: 	
+ Name :Prabin Rai
+ Roll: 0333
+ Output: Fred Jones
 */
 
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 int main()
 {
-	
+
+    string s;
+    string* p;
+    s = "Fred Jones";
+    p = &s;
+    cout << *p;
+
 }
